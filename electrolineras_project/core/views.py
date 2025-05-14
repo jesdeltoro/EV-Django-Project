09bcaaa083fa3654d4be2a0ed3c0f9cb802dad48
jesdeltoro <99795922+jesdeltoro@users.noreply.github.com>
2025-05-14 13:33:02 +0200

    Añadido logo y pantalla inicio

--- electrolineras_project/core/views.py
+++ electrolineras_project/core/views.py
@@ -2,6 +2,11 @@ from django.views.generic.base import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "core/home.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = "Electrolineras"
+        context['page_description'] = "Electrolineras en España"
+        return context
 
 class SamplePageView(TemplateView):
     template_name = "core/sample.html"
