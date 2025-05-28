308ce3aea6c3c605722d9f427019362b4e735c17
jesdeltoro <99795922+jesdeltoro@users.noreply.github.com>
2025-05-28 07:39:45 +0200

    warning pages

--- electrolineras_project/core/urls.py
+++ electrolineras_project/core/urls.py
@@ -9,7 +9,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('', HomePageView.as_view(), name="home"),
     path('mapa/', MapaPageView.as_view(), name="mapa"),
-    path('pages/', include('pages.urls')),  # This includes all URLs from the pages app
+    path('pages/', include(('pages.urls', 'core_pages_app'), namespace='core_pages_app')),  # Updated namespace to avoid conflict
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
